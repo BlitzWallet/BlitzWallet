acce1d36d8c551f2ead62c2f8b8e6eda4558115d
Blake Kaufman <blakekaufman9@gmail.com>
2025-02-28 11:37:47 -0500

    fixed style bug

--- app/components/admin/homeComponents/settingsContent/posPath/totalTipsScreen.js
+++ app/components/admin/homeComponents/settingsContent/posPath/totalTipsScreen.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
   entryRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginVertical: 10,
+    paddingVertical: 10,
     borderBlockColor: COLORS.halfModalBackgroundColor,
     borderBottomWidth: 1,
   },
