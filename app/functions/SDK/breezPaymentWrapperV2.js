bd2ef8bbce9b60d6c1d960a3713b911d3dc74643
Blake Kaufman <blakekaufman9@gmail.com>
2025-03-18 15:11:00 -0400

    created new app store payment functions

---
+++ app/functions/SDK/breezPaymentWrapperV2.js
@@ -0,0 +1,50 @@
+import {
+  reportIssue,
+  ReportIssueRequestVariant,
+  sendPayment,
+} from '@breeztech/react-native-breez-sdk';
+import {getLocalStorageItem} from '../localStorage';
+
+export default async function breezPaymentWrapperV2({
+  paymentInfo,
+  amountMsat,
+  paymentDescription = '',
+}) {
+  let response;
+  try {
+    const useTrampoline =
+      JSON.parse(await getLocalStorageItem('useTrampoline')) ?? true;
+
+    console.log('USING TRAMPOLINE', useTrampoline);
+
+    response = !!paymentInfo?.invoice?.amountMsat
+      ? await sendPayment({
+          useTrampoline: useTrampoline,
+          bolt11: paymentInfo?.invoice?.bolt11,
+          label: paymentDescription,
+        })
+      : await sendPayment({
+          useTrampoline: useTrampoline,
+          bolt11: paymentInfo?.invoice?.bolt11,
+          amountMsat,
+          label: paymentDescription,
+        });
+    if (!!response.payment.error)
+      throw new Error(String(response.payment.error));
+    return {didWork: true, response};
+  } catch (err) {
+    console.log(err.message, 'PAYMENT FAILURE ERRROR');
+    console.log(response, 'PAYMENT RESPONSE ERRROR');
+    try {
+      const paymentHash = paymentInfo.invoice.paymentHash;
+      await reportIssue({
+        type: ReportIssueRequestVariant.PAYMENT_FAILURE,
+        data: {paymentHash},
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      return {didWork: false, response: {...response, reason: err.message}};
+    }
+  }
+}
---
+++ app/functions/apps/payments.js
@@ -0,0 +1,110 @@
+import {parseInput} from '@breeztech/react-native-breez-sdk';
+import {LIQUID_DEFAULT_FEE} from '../../constants';
+import {calculateBoltzFeeNew} from '../boltz/boltzFeeNew';
+import {breezLiquidPaymentWrapper} from '../breezLiquid';
+import {getMeltQuote, payLnInvoiceFromEcash} from '../eCash/wallet';
+import {getStoredProofs} from '../eCash/db';
+import {sumProofsValue} from '../eCash/proofs';
+import breezPaymentWrapperV2 from '../SDK/breezPaymentWrapperV2';
+
+export default async function sendStorePayment({
+  liquidNodeInformation,
+  nodeInformation,
+  invoice, // Bolt11 invoice
+  payingStateUpdate,
+  minMaxLiquidSwapAmounts,
+  sendingAmountSats,
+  masterInfoObject,
+  description = '',
+}) {
+  try {
+    const liquidFee =
+      calculateBoltzFeeNew(
+        sendingAmountSats,
+        'liquid-ln',
+        minMaxLiquidSwapAmounts['submarineSwapStats'],
+      ) + LIQUID_DEFAULT_FEE;
+    const lightningFee = sendingAmountSats * 0.005 + 4;
+
+    // Try to pay with eCash
+    if (masterInfoObject.enabledEcash) {
+      console.log('Paying with ecash....');
+      try {
+        const storedProofs = await getStoredProofs();
+        const balance = sumProofsValue(storedProofs);
+        if (balance > sendingAmountSats + lightningFee) {
+          const meltQuote = await getMeltQuote(invoice);
+          if (!meltQuote) throw new Error('Not able to generate ecash quote');
+          const didPay = await payLnInvoiceFromEcash({
+            quote: meltQuote.quote,
+            invoice,
+            proofsToUse: meltQuote.proofsToUse,
+            description: description,
+          });
+          if (!didPay.didWork) throw new Error(didPay.message);
+          return {
+            didWork: true,
+            response: {
+              feeSat: didPay.txObject.fee,
+              amountSat: didPay.txObject.amount,
+              status: 'complete',
+              details: {error: ''},
+            },
+            formattingType: 'ecash',
+          };
+        }
+      } catch (err) {
+        console.warn('eCash payment failed:', err.message);
+      }
+    }
+
+    // Try to pay with Liquid
+    if (
+      liquidNodeInformation.userBalance > sendingAmountSats + liquidFee &&
+      sendingAmountSats > minMaxLiquidSwapAmounts.min
+    ) {
+      console.log('Paying with liquid....');
+      const paymentResponse = await breezLiquidPaymentWrapper({
+        invoice: invoice,
+        paymentType: 'bolt11',
+      });
+
+      if (!paymentResponse.didWork)
+        throw new Error('Error occurred when paying invoice.');
+
+      return {
+        didWork: true,
+        response: paymentResponse.payment,
+        formattingType: 'liquidNode',
+      };
+    }
+
+    // Try to pay with Lightning
+    if (
+      masterInfoObject.liquidWalletSettings.isLightningEnabled &&
+      nodeInformation.userBalance > sendingAmountSats + lightningFee
+    ) {
+      console.log('Paying with lightning....');
+      const parsedLnInvoice = await parseInput(invoice);
+      const paymentResponse = await breezPaymentWrapperV2({
+        paymentInfo: parsedLnInvoice,
+        amountMsat: parsedLnInvoice?.invoice?.amountMsat,
+        paymentDescription: description,
+      });
+
+      if (!paymentResponse.didWork)
+        throw new Error('Error occurred when paying invoice.');
+      return {
+        didWork: true,
+        response: paymentResponse.response,
+        formattingType: 'lightningNode',
+      };
+    }
+
+    // No available balances for the purchase
+    throw new Error('No available balances for your purchase.');
+  } catch (err) {
+    console.log('Send store payment error:', err.message);
+    return {didWork: false, reason: err.message};
+  }
+}
