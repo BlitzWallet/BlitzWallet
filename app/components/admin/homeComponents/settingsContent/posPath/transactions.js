06d7dc46b88bb88c54eb8659ec7874bfc8595490
Blake Kaufman <blakekaufman9@gmail.com>
2025-03-25 11:33:25 -0700

    moved remove employee to detials page

--- app/components/admin/homeComponents/settingsContent/posPath/transactions.js
+++ app/components/admin/homeComponents/settingsContent/posPath/transactions.js
@@ -1,28 +1,20 @@
 import {useCallback, useMemo, useState} from 'react';
-import {CENTER, ICONS, SIZES} from '../../../../../constants';
+import {CENTER, ICONS} from '../../../../../constants';
 import {
   GlobalThemeView,
   ThemeText,
 } from '../../../../../functions/CustomElements';
 import CustomSettingsTopBar from '../../../../../functions/CustomElements/settingsTopBar';
 import CustomSearchInput from '../../../../../functions/CustomElements/searchInput';
-import {
-  FlatList,
-  Platform,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {FlatList, Platform, StyleSheet, View} from 'react-native';
 import CustomButton from '../../../../../functions/CustomElements/button';
 import {useNavigation} from '@react-navigation/native';
 import {usePOSTransactions} from '../../../../../../context-store/pos';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {ANDROIDSAFEAREA} from '../../../../../constants/styles';
-import {INSET_WINDOW_WIDTH} from '../../../../../constants/theme';
 import displayCorrectDenomination from '../../../../../functions/displayCorrectDenomination';
 import {useGlobalContextProvider} from '../../../../../../context-store/context';
 import {useNodeContext} from '../../../../../../context-store/nodeContext';
-import {deleteEmployee} from '../../../../../functions/pos';
 
 export default function ViewPOSTransactions() {
   const {groupedTxs} = usePOSTransactions();
@@ -45,52 +37,34 @@ export default function ViewPOSTransactions() {
           const [name, info] = tx;
           return name.toLowerCase()?.startsWith(employeeName.toLowerCase());
         });
-  }, [groupedTxs]);
-
-  const removeEmployee = useCallback(async name => {
-    navigate.navigate('ConfirmActionPage', {
-      confirmMessage: 'Are you sure you want to remove this employee?',
-      confirmFunction: () => deleteEmployee(name),
-    });
-  }, []);
+  }, [groupedTxs, employeeName]);
 
   const transactionItem = useCallback(({item}) => {
     const [name, {totalTipAmount}] = item;
-
     return (
-      <TouchableOpacity
-        onLongPress={() => removeEmployee(name)}
-        activeOpacity={1}>
-        <View style={styles.transactionContainer}>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              marginRight: 'auto',
-            }}>
-            <View style={styles.nameContainer}>
-              <ThemeText
-                styles={styles.nameText}
-                content={name}
-                CustomNumberOfLines={1}
-              />
-            </View>
+      <View style={styles.transactionContainer}>
+        <View style={styles.nameAndTipContainer}>
+          <View style={styles.nameContainer}>
             <ThemeText
-              content={`Unpaid tips: ${displayCorrectDenomination({
-                amount: totalTipAmount,
-                masterInfoObject,
-                nodeInformation,
-              })}`}
+              styles={styles.nameText}
+              content={name}
               CustomNumberOfLines={1}
             />
           </View>
-          <CustomButton
-            actionFunction={() => navigate.navigate('TotalTipsScreen', {item})}
-            textContent={'View'}
+          <ThemeText
+            content={`Unpaid tips: ${displayCorrectDenomination({
+              amount: totalTipAmount,
+              masterInfoObject,
+              nodeInformation,
+            })}`}
+            CustomNumberOfLines={1}
           />
         </View>
-      </TouchableOpacity>
+        <CustomButton
+          actionFunction={() => navigate.navigate('TotalTipsScreen', {item})}
+          textContent={'View'}
+        />
+      </View>
     );
   }, []);
 
@@ -104,13 +78,13 @@ export default function ViewPOSTransactions() {
         containerStyles={{marginBottom: 0}}
         label={'Tips to pay'}
       />
-      <CustomSearchInput
-        placeholderText={'Employee name'}
-        setInputText={setEmployeeName}
-        containerStyles={{marginTop: 10}}
-      />
-      {filteredList.length ? (
-        <View style={{...styles.container, alignItems: 'center'}}>
+      <View style={{...styles.container, alignItems: 'center'}}>
+        <CustomSearchInput
+          placeholderText={'Employee name'}
+          setInputText={setEmployeeName}
+          containerStyles={{marginTop: 10}}
+        />
+        {filteredList.length ? (
           <FlatList
             style={{width: '100%'}}
             contentContainerStyle={{paddingBottom: paddingBottom + 50}}
@@ -120,24 +94,10 @@ export default function ViewPOSTransactions() {
             renderItem={transactionItem}
             keyExtractor={([name, {total, txs}]) => name}
           />
-          <CustomButton
-            buttonStyles={{
-              width: 'auto',
-              ...CENTER,
-              position: 'absolute',
-              bottom: paddingBottom,
-            }}
-            actionFunction={() => {
-              navigate.navigate('TotalTipsScreen');
-            }}
-            textContent={'Employee Tip Totals'}
-          />
-        </View>
-      ) : (
-        <View style={{flex: 1, alignItems: 'center'}}>
-          <ThemeText styles={{marginTop: 10}} content={'No transactions '} />
-        </View>
-      )}
+        ) : (
+          <ThemeText styles={{marginTop: 20}} content={'No tips'} />
+        )}
+      </View>
     </GlobalThemeView>
   );
 }
@@ -145,11 +105,12 @@ export default function ViewPOSTransactions() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '95%',
+    ...CENTER,
   },
   transactionContainer: {
     marginVertical: 12,
-    width: INSET_WINDOW_WIDTH,
-    ...CENTER,
+    width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
   },
@@ -164,5 +125,11 @@ const styles = StyleSheet.create({
   image: {
     height: 12,
     width: 12,
+  },
+  nameAndTipContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginRight: 'auto',
   },
 });
