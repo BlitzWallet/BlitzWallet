7d83bb27cad337dc513c6614dbcba73e7188ecac
Blake Kaufman <blakekaufman9@gmail.com>
2025-04-15 11:06:49 -0400

    fixed charttime style bug

--- app/components/admin/homeComponents/explore/dateCountdown.js
+++ app/components/admin/homeComponents/explore/dateCountdown.js
@@ -1,7 +1,8 @@
 import {ThemeText} from '../../../../functions/CustomElements';
 import {useFocusEffect} from '@react-navigation/native';
 import {useState, useRef, useCallback, useMemo} from 'react';
-import {InteractionManager} from 'react-native';
+import {InteractionManager, StyleSheet} from 'react-native';
+import {SIZES} from '../../../../constants';
 
 export default function DateCountdown() {
   const [minuteTick, setMinuteTick] = useState();
@@ -16,22 +17,6 @@ export default function DateCountdown() {
       }
 
       intervalRef.current = setInterval(() => {
-        const timestamp = new Date().getTime();
-        const date = new Date(timestamp);
-
-        // Get midnight of the same day
-        const midnight = new Date(date);
-        midnight.setUTCHours(24, 0, 0, 0); // Set to midnight (start of next day)
-
-        // Calculate time difference in milliseconds
-        const diffMs = midnight - date;
-
-        // Convert to hours, minutes, and seconds
-        const totalSeconds = Math.floor(diffMs / 1000);
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-
         setMinuteTick(getFommattedTime());
       }, 1000);
 
@@ -50,7 +35,7 @@ export default function DateCountdown() {
   return (
     <ThemeText
       CustomNumberOfLines={1}
-      styles={{fontSize: 'small', marginRight: 5, flex: 1}}
+      styles={styles.dateText}
       content={`(${minuteTick} left)`}
     />
   );
@@ -74,3 +59,11 @@ function getFommattedTime() {
   const seconds = totalSeconds % 60;
   return `${hours}h ${minutes}m ${seconds}s`;
 }
+const styles = StyleSheet.create({
+  dateText: {
+    fontSize: SIZES.small,
+    marginRight: 5,
+    flex: 1,
+    includeFontPadding: false,
+  },
+});
