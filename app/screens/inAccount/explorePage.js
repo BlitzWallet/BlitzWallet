fd3c84a1e055823bf7c6c5874c24b3fb65c9c78d
Blake Kaufman <blakekaufman9@gmail.com>
2025-04-15 11:06:31 -0400

    finalized explore page

--- app/screens/inAccount/explorePage.js
+++ app/screens/inAccount/explorePage.js
@@ -1,279 +1,279 @@
-import {GlobalThemeView, ThemeText} from '../../functions/CustomElements';
-import React, {useMemo, useState, useEffect} from 'react';
-import {Grid, LineChart, XAxis, YAxis} from 'react-native-svg-charts';
+import {ThemeText} from '../../functions/CustomElements';
+import React, {useMemo, useState} from 'react';
+import {LineChart, XAxis, YAxis} from 'react-native-svg-charts';
 import {ScrollView, StyleSheet, TouchableOpacity, View} from 'react-native';
 import GetThemeColors from '../../hooks/themeColors';
-import {
-  BLITZ_GOAL_USER_COUNT,
-  CENTER,
-  COLORS,
-  ICONS,
-  SIZES,
-} from '../../constants';
-import {Circle} from 'react-native-svg';
+import {BLITZ_GOAL_USER_COUNT, CENTER, COLORS, SIZES} from '../../constants';
 import DateCountdown from '../../components/admin/homeComponents/explore/dateCountdown';
 import {
+  DAY_IN_MILLS,
   MONTH_GROUPING,
+  MONTH_IN_MILLS,
+  WEEK_IN_MILLS,
   WEEK_OPTIONS,
+  YEAR_IN_MILLS,
 } from '../../components/admin/homeComponents/explore/constants';
-import ThemeImage from '../../functions/CustomElements/themeImage';
-import {INSET_WINDOW_WIDTH} from '../../constants/theme';
 import {formatBalanceAmount} from '../../functions';
-const Decorator = ({x, y, data}) => {
-  return data.map((value, index) => (
-    <Circle
-      key={index}
-      cx={x(index)}
-      cy={y(value)}
-      r={3}
-      stroke={COLORS.primary}
-      strokeWidth={1}
-      fill={'white'}
-    />
-  ));
-};
+import {useGlobalContextProvider} from '../../../context-store/context';
+import NoDataView from '../../components/admin/homeComponents/explore/noDataView';
+import Grid from '../../functions/CustomElements/chart/grid';
+import {INSET_WINDOW_WIDTH} from '../../constants/theme';
+import {useGlobalThemeContext} from '../../../context-store/theme';
+import LineChartDot from '../../functions/CustomElements/chart/lineChartDot';
+
 export default function ExploreUsers() {
   const [timeFrame, setTimeFrame] = useState('day');
-  const {backgroundOffset, textColor} = GetThemeColors();
-  const data = [
-    {date: 1744070400000, value: 998},
-    {date: 1744170400000, value: 1020},
-    {date: 1744270400000, value: 1037},
-    {date: 1744370400000, value: 1041},
-    {date: 1744470400000, value: 0},
-    {date: 1744570400000, value: 0},
-    {date: 1744670400000, value: 0},
-  ];
+  const {masterInfoObject} = useGlobalContextProvider();
+  const {backgroundOffset, textColor, backgroundColor} = GetThemeColors();
+  const {theme, darkModeType} = useGlobalThemeContext();
   const [targetUserCountBarWidth, setTargetUserCountBarWidth] = useState(0);
 
   const axesSvg = {fontSize: SIZES.small, fill: textColor};
   const verticalContentInset = {top: 10, bottom: 10};
-  const [xAxisHeight, setXAxisHeight] = useState(30);
+  const xAxisHeight = 30;
 
   const timeFrameElements = useMemo(() => {
     return ['day', 'week', 'month', 'year'].map(item => {
       return (
         <TouchableOpacity
+          key={item}
           onPress={() => setTimeFrame(item)}
           style={{
-            borderRadius: 8,
-            borderColor: COLORS.primary,
             backgroundColor:
-              item === timeFrame ? COLORS.primary : 'transparent',
-            borderWidth: 2,
+              item === timeFrame
+                ? theme && darkModeType
+                  ? COLORS.darkModeText
+                  : COLORS.primary
+                : 'transparent',
+            borderColor:
+              theme && darkModeType ? COLORS.darkModeText : COLORS.primary,
+            ...styles.timeFrameElement,
           }}>
           <ThemeText
             styles={{
-              color: item === timeFrame ? COLORS.darkModeText : textColor,
-              textTransform: 'capitalize',
-              padding: 10,
+              color:
+                item === timeFrame
+                  ? theme && darkModeType
+                    ? COLORS.lightModeText
+                    : COLORS.darkModeText
+                  : textColor,
+              ...styles.timeFrameElementText,
             }}
             content={item === 'day' ? 'Daily' : `${item}ly`}
           />
         </TouchableOpacity>
       );
     });
-  }, [timeFrame, textColor]);
+  }, [timeFrame, textColor, theme, darkModeType]);
 
   const XaxisElement = useMemo(() => {
     return (
       <XAxis
-        style={{height: xAxisHeight, position: 'abolute'}}
-        data={data.map((_, index) => index)}
+        style={{
+          height: xAxisHeight,
+          // position: 'abolute',
+        }}
+        data={[0, 1, 2, 3, 4, 5, 6].map((_, index) => index)}
         formatLabel={(value, index) => {
           if (timeFrame === 'year') {
-            const YEAR_IN_MILLS = 1000 * 60 * 60 * 24 * 365;
             const now = new Date().getTime();
             return `${new Date(
               now - YEAR_IN_MILLS * Math.abs(6 - index),
             ).getFullYear()}`;
           } else if (timeFrame === 'month') {
-            const MONTH_IN_MILLS = 1000 * 60 * 60 * 24 * 31;
             const now = new Date().getTime();
             const dateIndex = new Date(
               now - MONTH_IN_MILLS * Math.abs(6 - index),
             ).getMonth();
-            return [
-              'Jan',
-              'Feb',
-              'Mar',
-              'Apr',
-              'May',
-              'Jun',
-              'Jul',
-              'Aug',
-              'Sep',
-              'Oct',
-              'Nov',
-              'Dec',
-            ][dateIndex];
+            return MONTH_GROUPING[dateIndex];
           } else if (timeFrame === 'day') {
-            const DAY_IN_MILLS = 1000 * 60 * 60 * 24;
-            const now = new Date().getTime();
+            const now = new Date().getTime() - DAY_IN_MILLS;
             const dateIndex = new Date(
-              now - DAY_IN_MILLS * Math.abs(6 - index),
+              now - DAY_IN_MILLS * Math.abs(7 - index),
             ).getDay();
-            return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][dateIndex];
+            return WEEK_OPTIONS[dateIndex];
           } else {
-            const WEEK_IN_MILLS = 1000 * 60 * 60 * 24 * 7;
-            const now = new Date().getTime();
+            const pretendWeekToday = new Date().getTime() - WEEK_IN_MILLS;
             const dateIndex = new Date(
-              now - WEEK_IN_MILLS * Math.abs(6 - index),
+              pretendWeekToday - WEEK_IN_MILLS * Math.abs(6 - index),
             );
             const day = dateIndex.getDate();
             const month = dateIndex.getMonth();
-
-            return `${MONTH_GROUPING[month]} ${day}`;
+            return `${month}/${day}`;
           }
         }}
         contentInset={{left: 15, right: 15}}
         svg={{
           ...axesSvg,
-          rotation: timeFrame === 'week' ? -30 : 0,
-          origin: timeFrame === 'week' ? 15 : 0,
-          y: timeFrame === 'week' ? 10 : 0,
+          rotation: 0,
+          origin: 0,
+          y: 0,
         }}
       />
     );
-  }, [timeFrame, xAxisHeight]);
-
-  useEffect(() => {
-    const labelFontSize = SIZES.small;
-    const labelRotation = 45;
-    let longestLabel = '';
-
-    if (timeFrame === 'month') {
-      longestLabel = 'Jan';
-    } else if (timeFrame === 'day') {
-      longestLabel = 'Mon';
-    } else if (timeFrame === 'week') {
-      longestLabel = 'Mon 21';
-    } else {
-      longestLabel = '2024';
-    }
-
-    const averageCharWidth = labelFontSize * 0.6;
-    const labelPixelLength = longestLabel.length * averageCharWidth;
-    const estimatedHeight =
-      Math.sin((labelRotation * Math.PI) / 180) * labelPixelLength + 10;
-
-    setXAxisHeight(timeFrame !== 'week' ? 30 : estimatedHeight + 15);
   }, [timeFrame]);
 
+  if (
+    !masterInfoObject.exploreData ||
+    !Object.keys(masterInfoObject.exploreData).length
+  ) {
+    return <NoDataView />;
+  }
+  const dataObject = JSON.parse(JSON.stringify(masterInfoObject.exploreData));
+  const data = dataObject ? dataObject[timeFrame].reverse() : [];
+
+  const totalToday = masterInfoObject.exploreData['day'][0].value;
+  const totalYesterday = masterInfoObject.exploreData['day'][1].value;
+
   return (
-    <GlobalThemeView useStandardWidth={true} styles={{paddingBottom: 0}}>
-      <ThemeText content={'Explore'} styles={{...styles.headerText}} />
-      <ScrollView
-        contentContainerStyle={{
-          width: '95%',
-          paddingTop: 20,
-          ...CENTER,
-        }}>
-        <View
-          style={{
-            backgroundColor: backgroundOffset,
-            borderRadius: 8,
-            padding: 10,
-          }}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.scrollView}>
+      <View style={{...styles.statsCard, backgroundColor: backgroundOffset}}>
+        <ThemeText
+          styles={styles.statsCardHeader}
+          content={'Blitz Wallet Downloads'}
+        />
+        <ThemeText styles={{marginBottom: 5}} content={'Today'} />
+        <View style={styles.statsCardHorizontal}>
+          <DateCountdown />
           <ThemeText
-            styles={{
-              fontSize: SIZES.xLarge,
-              textAlign: 'center',
-              marginBottom: 5,
-            }}
-            content={'Blitz Wallet Downloads'}
+            styles={styles.statsCardNumberText}
+            content={`${formatBalanceAmount(
+              totalToday,
+            )} of ${formatBalanceAmount(BLITZ_GOAL_USER_COUNT)} (${(
+              (totalToday / BLITZ_GOAL_USER_COUNT) *
+              100
+            ).toFixed(4)}%)`}
           />
-          <ThemeText styles={{marginBottom: 5}} content={'Today'} />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <DateCountdown />
-
-            <ThemeText
-              styles={{fontSize: 'small'}}
-              content={`${formatBalanceAmount(1050)} of ${formatBalanceAmount(
-                BLITZ_GOAL_USER_COUNT,
-              )} (${(1050 / BLITZ_GOAL_USER_COUNT) * 100}%)`}
-            />
-          </View>
-          <View
-            onLayout={event => {
-              setTargetUserCountBarWidth(event.nativeEvent.layout.width);
-            }}
-            style={{
-              height: 30,
-              width: '100%',
-              backgroundColor: COLORS.lightModeBackground,
-              borderRadius: 25,
-              marginVertical: 10,
-              overflow: 'hidden',
-            }}>
-            <View
-              style={{
-                width:
-                  targetUserCountBarWidth * (10000 / BLITZ_GOAL_USER_COUNT),
-                height: '100%',
-                backgroundColor: COLORS.primary,
-                marginRight: 'auto',
-              }}></View>
-          </View>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <ThemeText content={'Yesterday'} />
-            <ThemeText
-              styles={{fontSize: 'small'}}
-              content={`${formatBalanceAmount(50)} of ${formatBalanceAmount(
-                BLITZ_GOAL_USER_COUNT,
-              )} (${((50 / BLITZ_GOAL_USER_COUNT) * 100).toFixed(4)}%)`}
-            />
-          </View>
         </View>
-
         <View
+          onLayout={event => {
+            setTargetUserCountBarWidth(event.nativeEvent.layout.width);
+          }}
           style={{
-            height: 250,
-            flexDirection: 'row',
-            marginTop: 20,
+            backgroundColor: backgroundColor,
+            ...styles.statsCardBar,
           }}>
-          <YAxis
+          <View
+            style={{
+              width:
+                targetUserCountBarWidth * (totalToday / BLITZ_GOAL_USER_COUNT),
+              backgroundColor:
+                theme && darkModeType ? COLORS.darkModeText : COLORS.primary,
+              ...styles.statsCardBarFill,
+            }}
+          />
+        </View>
+        <View style={styles.statsCardHorizontal}>
+          <ThemeText content={'Yesterday'} />
+          <ThemeText
+            styles={styles.statsCardNumberText}
+            content={`${formatBalanceAmount(
+              totalYesterday,
+            )} of ${formatBalanceAmount(BLITZ_GOAL_USER_COUNT)} (${(
+              (totalYesterday / BLITZ_GOAL_USER_COUNT) *
+              100
+            ).toFixed(4)}%)`}
+          />
+        </View>
+      </View>
+
+      <View style={styles.chartContainer}>
+        <YAxis
+          data={data.map(data => data.value)}
+          style={{marginBottom: xAxisHeight}}
+          contentInset={verticalContentInset}
+          svg={axesSvg}
+          min={data[0].value * 0.95}
+          max={totalToday * 1.05}
+          numberOfTicks={7}
+        />
+        <View style={{flex: 1, marginLeft: 5}}>
+          <LineChart
+            style={{flex: 1}}
             data={data.map(data => data.value)}
-            style={{marginBottom: xAxisHeight}}
-            contentInset={verticalContentInset}
-            svg={axesSvg}
-          />
-          <View style={{flex: 1, marginLeft: 5}}>
-            <LineChart
-              style={{flex: 1}}
-              data={data.map(data => data.value)}
-              contentInset={{...verticalContentInset, left: 10, right: 10}}
-              svg={{stroke: COLORS.primary, strokeWidth: 3}}>
-              <Grid />
-              <Decorator />
-            </LineChart>
-            {XaxisElement}
-          </View>
+            contentInset={{...verticalContentInset, left: 10, right: 10}}
+            svg={{
+              stroke:
+                theme && darkModeType ? COLORS.darkModeText : COLORS.primary,
+              strokeWidth: 3,
+            }}
+            yMin={data[0].value * 0.95}
+            yMax={totalToday * 1.05}>
+            <Grid />
+            <LineChartDot
+              color={
+                theme && darkModeType ? COLORS.darkModeText : COLORS.primary
+              }
+            />
+          </LineChart>
+          {XaxisElement}
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '90%',
-            ...CENTER,
-          }}>
-          {timeFrameElements}
-        </View>
-      </ScrollView>
-    </GlobalThemeView>
+      </View>
+      <View style={styles.timeFrameElementsContainer}>{timeFrameElements}</View>
+    </ScrollView>
   );
 }
 
-function yearElements() {
-  const currentYear = new Date().getFullYear();
-  const YEAR_OPTIONS = Array.from({length: 7}, (_, i) => currentYear - i);
-  return YEAR_OPTIONS.reverse();
-}
-
 const styles = StyleSheet.create({
-  headerText: {fontSize: SIZES.large, ...CENTER},
+  scrollView: {
+    width: INSET_WINDOW_WIDTH,
+    paddingTop: 20,
+    ...CENTER,
+  },
+  statsCard: {borderRadius: 8, padding: 10},
+  statsCardHeader: {
+    fontSize: SIZES.xLarge,
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  statsCardHorizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  statsCardBar: {
+    height: 30,
+    width: '100%',
+    borderRadius: 25,
+    marginVertical: 10,
+    overflow: 'hidden',
+  },
+  statsCardBarFill: {
+    height: '100%',
+    marginRight: 'auto',
+  },
+  statsCardNumberText: {
+    fontSize: SIZES.small,
+    includeFontPadding: false,
+  },
+  chartContainer: {
+    height: 250,
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  timeFrameElementsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    width: '100%',
+    rowGap: 10,
+    columnGap: 10,
+    flexWrap: 'wrap',
+    ...CENTER,
+  },
+  timeFrameElement: {
+    flexGrow: 1,
+    borderRadius: 8,
+
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  timeFrameElementText: {
+    textTransform: 'capitalize',
+    includeFontPadding: false,
+    padding: 10,
+  },
 });
