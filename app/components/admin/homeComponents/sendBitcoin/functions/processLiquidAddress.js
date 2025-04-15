69dfcf73f75d08cbeeeff782244ce9fd42c18a61
Blake Kaufman <blakekaufman9@gmail.com>
2025-04-15 14:26:46 -0400

    fixed liquid decode process from new parse method

--- app/components/admin/homeComponents/sendBitcoin/functions/processLiquidAddress.js
+++ app/components/admin/homeComponents/sendBitcoin/functions/processLiquidAddress.js
@@ -3,12 +3,9 @@ import {
   crashlyticsLogReport,
   crashlyticsRecordErrorReport,
 } from '../../../../../functions/crashlyticsLogs';
-import bip39LiquidAddressDecode from './bip39LiquidAddressDecode';
 
 export default function processLiquidAddress(input, context) {
   const {
-    btcAddress,
-    setPaymentInfo,
     liquidNodeInformation,
     nodeInformation,
     masterInfoObject,
@@ -17,19 +14,19 @@ export default function processLiquidAddress(input, context) {
   } = context;
   try {
     crashlyticsLogReport('Handling decode liquid address');
-    let addressInfo = bip39LiquidAddressDecode(
-      btcAddress,
-      liquidNodeInformation,
-    );
+    let addressInfo = JSON.parse(JSON.stringify(input?.address));
     if (comingFromAccept) {
       addressInfo.amount = enteredPaymentInfo.amount;
       addressInfo.label = enteredPaymentInfo.description;
+      addressInfo.message = enteredPaymentInfo.description;
       addressInfo.isBip21 = true;
       const shouldDrain =
         liquidNodeInformation.userBalance - addressInfo.amount < 10
           ? true
           : false;
       addressInfo.shouldDrain = shouldDrain;
+    } else {
+      addressInfo.amount = addressInfo.amountSat;
     }
 
     const amountSat = addressInfo.amount;
