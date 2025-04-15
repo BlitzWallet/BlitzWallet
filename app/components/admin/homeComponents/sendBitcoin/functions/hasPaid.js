d239b2f72e092a346f5799d14e2bd0696c3c28f5
Blake Kaufman <blakekaufman9@gmail.com>
2025-04-15 14:25:17 -0400

    fixed checking paid invoices for bolt12

--- app/components/admin/homeComponents/sendBitcoin/functions/hasPaid.js
+++ app/components/admin/homeComponents/sendBitcoin/functions/hasPaid.js
@@ -11,15 +11,18 @@ export default function hasAlredyPaidInvoice({
 }) {
   try {
     crashlyticsLogReport('Begining already paid invoice function');
-    const didPayWithLiquid = liquidNodeInformation.transactions.find(
-      tx =>
-        tx?.destination === scannedAddress && tx?.details?.type === 'lightning',
-    );
+    const didPayWithLiquid = liquidNodeInformation.transactions.find(tx => {
+      return (
+        tx?.destination === scannedAddress?.trim() &&
+        tx?.details?.type === 'lightning' &&
+        !tx?.details?.bolt12Offer
+      );
+    });
     const didPayWithLightning = nodeInformation.transactions.find(
-      tx => tx.details.data.bolt11 === scannedAddress,
+      tx => tx.details.data.bolt11 === scannedAddress?.trim(),
     );
     const didPayWitheCash = ecashWalletInformation.transactions?.find(
-      tx => tx?.invoice === scannedAddress,
+      tx => tx?.invoice === scannedAddress?.trim(),
     );
     return !!didPayWithLiquid || !!didPayWithLightning || !!didPayWitheCash;
   } catch (err) {
