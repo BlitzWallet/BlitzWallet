09fc8a5d9419ddcd2cc0172606b0b19acc79f79f
Blake Kaufman <blakekaufman9@gmail.com>
2025-04-16 18:12:04 -0400

    added support for blink qr codes

--- app/functions/sendBitcoin/index.js
+++ app/functions/sendBitcoin/index.js
@@ -3,6 +3,7 @@ import {convertMerchantQRToLightningAddress} from './getMerchantAddress';
 import {getImageFromLibrary} from '../imagePickerWrapper';
 import RNQRGenerator from 'rn-qr-generator';
 import getClipboardText from '../getClipboardText';
+import testURLForInvoice from '../testURLForInvoice';
 
 async function navigateToSendUsingClipboard(navigate, callLocation, from) {
   const response = await getClipboardText();
@@ -11,27 +12,36 @@ async function navigateToSendUsingClipboard(navigate, callLocation, from) {
     navigate.navigate('ErrorScreen', {errorMessage: response.reason});
     return;
   }
-  const data = response.data;
+  const clipboardData = response.data?.trim();
+  let data;
 
-  if (WEBSITE_REGEX.test(data)) {
-    navigate.navigate('CustomWebView', {
-      headerText: '',
-      webViewURL: data,
+  if (WEBSITE_REGEX.test(clipboardData)) {
+    const invoice = testURLForInvoice(clipboardData);
+
+    if (!invoice) {
+      navigate.navigate('CustomWebView', {
+        headerText: '',
+        webViewURL: clipboardData,
+      });
+      return;
+    }
+    data = invoice;
+  } else {
+    const merchantLNAddress = convertMerchantQRToLightningAddress({
+      qrContent: clipboardData,
+      network: process.env.BOLTZ_ENVIRONEMNT,
     });
-    return;
+    data = merchantLNAddress;
   }
-  const merchantLNAddress = convertMerchantQRToLightningAddress({
-    qrContent: data,
-    network: process.env.BOLTZ_ENVIRONEMNT,
-  });
+
   if (from === 'home')
     navigate.navigate('ConfirmPaymentScreen', {
-      btcAdress: merchantLNAddress || data,
+      btcAdress: data || clipboardData,
       fromPage: callLocation === 'slideCamera' ? 'slideCamera' : '',
     });
   else
     navigate.replace('ConfirmPaymentScreen', {
-      btcAdress: merchantLNAddress || data,
+      btcAdress: data || clipboardData,
       fromPage: callLocation === 'slideCamera' ? 'slideCamera' : '',
     });
 }
@@ -77,11 +87,16 @@ async function getQRImage(navigate, callLocation) {
   }
 
   if (WEBSITE_REGEX.test(address)) {
-    navigate.navigate('CustomWebView', {
-      headerText: '',
-      webViewURL: address,
-    });
-    return {btcAdress: '', didWork: false, error: ''};
+    const invoice = testURLForInvoice(address);
+
+    if (!invoice) {
+      navigate.navigate('CustomWebView', {
+        headerText: '',
+        webViewURL: address,
+      });
+      return {btcAdress: '', didWork: false, error: ''};
+    }
+    return {btcAdress: invoice, didWork: true, error: ''};
   }
   const merchantLNAddress = convertMerchantQRToLightningAddress({
     qrContent: address,
