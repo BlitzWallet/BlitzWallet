30e47f9169fda766483c05b3b8d80623d85e1d10
Blake Kaufman <blakekaufman9@gmail.com>
2025-04-28 15:10:08 -0400

    fixed contact payment message not shwoing up

--- app/components/admin/homeComponents/sendBitcoin/functions/processLiquidAddress.js
+++ app/components/admin/homeComponents/sendBitcoin/functions/processLiquidAddress.js
@@ -17,8 +17,10 @@ export default function processLiquidAddress(input, context) {
     let addressInfo = JSON.parse(JSON.stringify(input?.address));
     if (comingFromAccept) {
       addressInfo.amount = enteredPaymentInfo.amount;
-      addressInfo.label = enteredPaymentInfo.description;
-      addressInfo.message = enteredPaymentInfo.description;
+      addressInfo.label =
+        enteredPaymentInfo.description || input?.address?.label || '';
+      addressInfo.message =
+        enteredPaymentInfo.description || input?.address?.message || '';
       addressInfo.isBip21 = true;
       const shouldDrain =
         liquidNodeInformation.userBalance - addressInfo.amount < 10
