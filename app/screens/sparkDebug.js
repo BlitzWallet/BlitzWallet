cc65e82793faef4475f15427780abd5e0d876909
Blake Kaufman <blakekaufman9@gmail.com>
2025-05-21 16:13:16 -0400

    adding spark test

---
+++ app/screens/sparkDebug.js
@@ -0,0 +1,13 @@
+import {SafeAreaView, Text, View, TouchableOpacity} from 'react-native';
+
+import {SparkWallet} from '@buildonspark/spark-sdk/native';
+
+export default function SparkDebug() {
+  // SDK events listener
+
+  return (
+    <SafeAreaView>
+      <Text>Testing</Text>
+    </SafeAreaView>
+  );
+}
