807dc595873679931b5f302e65fc761e2b4b2611
Blake Kaufman <blakekaufman9@gmail.com>
2025-07-05 14:51:11 -0400

    lnurl withdrawl right to spark

--- app/components/admin/homeComponents/sendBitcoin/functions/processLNUrlWithdrawl.js
+++ app/components/admin/homeComponents/sendBitcoin/functions/processLNUrlWithdrawl.js
@@ -1,15 +1,47 @@
-import {lnurlWithdraw} from '@breeztech/react-native-breez-sdk-liquid';
 import {crashlyticsLogReport} from '../../../../../functions/crashlyticsLogs';
+import {sparkReceivePaymentWrapper} from '../../../../../functions/spark/payments';
+import {getSparkLightningPaymentStatus} from '../../../../../functions/spark';
 
 export default async function processLNUrlWithdraw(input, context) {
   const {setLoadingMessage} = context;
   crashlyticsLogReport('Begining LNURL withdrawl process');
-  setLoadingMessage('Starting LNURL withdrawl');
+  setLoadingMessage('Generating invoice for withdrawl');
 
-  const amountMsat = input.data.minWithdrawable;
-  await lnurlWithdraw({
-    data: input.data,
-    amountMsat,
-    description: 'Withdrawl',
+  const minAmount = input.data.minWithdrawable;
+
+  const invoice = await sparkReceivePaymentWrapper({
+    amountSats: Math.round(minAmount / 1000),
+    memo: input.data.defaultDescription || '',
+    paymentType: 'lightning',
   });
+
+  if (!invoice.didWork)
+    throw new Error('Unable to generate invoice for lnurl withdrawl');
+
+  const callbackUrl = new URL(input.data.callback);
+  callbackUrl.searchParams.set('k1', input.data.k1);
+  callbackUrl.searchParams.set('pr', invoice.invoice);
+
+  const callbackResponse = await fetch(callbackUrl.toString());
+  const responseData = await callbackResponse.json();
+
+  if (responseData.status === 'ERROR') {
+    throw new Error(responseData.reason);
+  }
+  setLoadingMessage('Waiting for payment...');
+  await pollForResponse(invoice.data);
+}
+
+async function pollForResponse(invoiceData) {
+  let didFind = false;
+  let maxCount = 5;
+  let currentCount = 0;
+  while (!didFind && currentCount < maxCount) {
+    await new Promise(res => setTimeout(res, 5000));
+    const sparkReceiveResposne = await getSparkLightningPaymentStatus({
+      lightningInvoiceId: invoiceData.id,
+    });
+    if (sparkReceiveResposne.transfer) break;
+  }
+  await new Promise(res => setTimeout(res, 8000));
 }
