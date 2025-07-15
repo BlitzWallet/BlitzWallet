b0dfec2368159bfeacadf6ef261ce68c02d76842
Blake Kaufman <blakekaufman9@gmail.com>
2025-07-15 07:59:43 -0400

    fixing spark error message from running before user gets to homescreen

--- context-store/sparkConnection.js
+++ context-store/sparkConnection.js
@@ -1,19 +1,30 @@
 import {useNavigation} from '@react-navigation/native';
 import {useSparkWallet} from './sparkContext';
 import {useEffect} from 'react';
+import {useAppStatus} from './appStatus';
 
 export function SparkConnectionManager() {
   const {sparkConnectionError, sparkInformation} = useSparkWallet();
+  const {didGetToHomepage} = useAppStatus();
   const navigation = useNavigation();
 
   useEffect(() => {
-    if (sparkInformation.didConnect === false && sparkConnectionError) {
-      navigation.navigate('SparkErrorScreen', {
-        errorMessage:
-          sparkConnectionError || 'Spark connection failed. Please try again.',
+    if (
+      sparkInformation.didConnect === false &&
+      sparkConnectionError &&
+      didGetToHomepage
+    ) {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          navigation.navigate('SparkErrorScreen', {
+            errorMessage:
+              sparkConnectionError ||
+              'Spark connection failed. Please try again.',
+          });
+        });
       });
     }
-  }, [sparkInformation.didConnect, sparkConnectionError]);
+  }, [sparkInformation.didConnect, sparkConnectionError, didGetToHomepage]);
 
   return null;
 }
