cd207a6c93cc4c805dd1644294ec7d9010f1f39f
Blake Kaufman <blakekaufman9@gmail.com>
2025-07-16 12:22:46 -0400

    optimzed contacts page rerenders

---
+++ app/components/admin/homeComponents/contacts/contactsPageComponents/utilityFunctions.js
@@ -0,0 +1,43 @@
+// Utility functions remain the same
+export function createFormattedDate(time) {
+  const date = new Date(time);
+  const currentDate = new Date();
+  const differenceMs = currentDate - date;
+  const differenceDays = differenceMs / (1000 * 60 * 60 * 24);
+
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  let formattedTime;
+
+  if (differenceDays < 1) {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    formattedTime = `${formattedHours}:${formattedMinutes} ${ampm}`;
+  } else if (differenceDays < 2) {
+    formattedTime = 'Yesterday';
+  } else if (differenceDays <= 7) {
+    formattedTime = daysOfWeek[date.getDay()];
+  } else {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear() % 100;
+    formattedTime = `${month}/${day}/${year}`;
+  }
+
+  return formattedTime;
+}
+
+export function formatMessage(message) {
+  return message?.message?.description || ' ';
+}
