3a851f3258fc6be953015579afc4ac4eea13e8ea
Blake Kaufman <blakekaufman9@gmail.com>
2025-07-30 13:13:19 -0400

    adding lrc20 toggle to settings

--- app/components/admin/homeComponents/settingsContent/sparkLrc20.js
+++ app/components/admin/homeComponents/settingsContent/sparkLrc20.js
@@ -1,8 +1,10 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import CustomSettingsTopBar from '../../../../functions/CustomElements/settingsTopBar';
 import {GlobalThemeView} from '../../../../functions/CustomElements';
 import {useGlobalContextProvider} from '../../../../../context-store/context';
 import SettingsItemWithSlider from '../../../../functions/CustomElements/settings/settingsItemWithSlider';
+import {INSET_WINDOW_WIDTH} from '../../../../constants/theme';
+import {CENTER} from '../../../../constants';
 
 export default function SparkSettingsPage() {
   const {masterInfoObject, toggleMasterInfoObject} = useGlobalContextProvider();
@@ -10,12 +12,31 @@ export default function SparkSettingsPage() {
   return (
     <GlobalThemeView useStandardWidth={true}>
       <CustomSettingsTopBar label={'Spark Settings'} />
-      <SettingsItemWithSlider
-        settingsTitle={`${
-          lrc20Settings.isEnabled ? 'Enabled' : 'Disabled'
-        } LRC-20`}
-        settingDescription="LRC-20 is Spark’s native token. Enabling LRC-20 lets you send and receive tokens on the Spark network."
-      />
+      <View style={styles.container}>
+        <SettingsItemWithSlider
+          settingsTitle={`${
+            lrc20Settings.isEnabled ? 'Enabled' : 'Disabled'
+          } LRC-20`}
+          showDescription={true}
+          settingDescription="LRC-20 is Spark’s native token. Enabling LRC-20 lets you send and receive tokens on the Spark network."
+          handleSubmit={() =>
+            toggleMasterInfoObject({
+              lrc20Settings: {
+                ...lrc20Settings,
+                isEnabled: !lrc20Settings.isEnabled,
+              },
+            })
+          }
+          toggleSwitchStateValue={lrc20Settings.isEnabled}
+        />
+      </View>
     </GlobalThemeView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: INSET_WINDOW_WIDTH,
+    ...CENTER,
+  },
+});
