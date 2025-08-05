66d636ef03ed48405cdc16b6c7f089ffafb0ed8a
Blake Kaufman <blakekaufman9@gmail.com>
2025-08-05 14:07:23 -0400

    added donation message

--- app/components/admin/homeComponents/settingsContent/sparkLrc20.js
+++ app/components/admin/homeComponents/settingsContent/sparkLrc20.js
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import CustomSettingsTopBar from '../../../../functions/CustomElements/settingsTopBar';
 import {GlobalThemeView} from '../../../../functions/CustomElements';
 import {useGlobalContextProvider} from '../../../../../context-store/context';
@@ -9,9 +9,11 @@ import {useEffect, useRef} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import displayCorrectDenomination from '../../../../functions/displayCorrectDenomination';
 import {useNodeContext} from '../../../../../context-store/nodeContext';
+import {useSparkWallet} from '../../../../../context-store/sparkContext';
 
 export default function SparkSettingsPage() {
   const navigate = useNavigation();
+  const {sparkInformation} = useSparkWallet();
   const {masterInfoObject, toggleMasterInfoObject} = useGlobalContextProvider();
   const {fiatStats} = useNodeContext();
   const isInitialRender = useRef(true);
@@ -25,40 +27,48 @@ export default function SparkSettingsPage() {
     }
 
     if (!lrc20Settings.isEnabled) return;
+    if (sparkInformation.balance > 5) return;
     navigate.navigate('InformationPopup', {
-      textContent: `Blitz adds a ${displayCorrectDenomination({
+      textContent: `Your current wallet balance is ${displayCorrectDenomination(
+        {
+          amount: sparkInformation.balance,
+          masterInfoObject,
+          fiatStats,
+        },
+      )}. Blitz adds a ${displayCorrectDenomination({
         amount: 5,
         masterInfoObject,
         fiatStats,
       })} fee to all LRC-20 payments. Make sure you have some Bitcoin in your wallet to send tokens.`,
       buttonText: 'I understand',
     });
-    console.log(lrc20Settings, 't');
-  }, [lrc20Settings]);
+  }, [lrc20Settings, sparkInformation.balance]);
   return (
     <GlobalThemeView useStandardWidth={true}>
       <CustomSettingsTopBar label={'Spark Settings'} />
-      <View style={styles.container}>
-        <SettingsItemWithSlider
-          settingsTitle={`${
-            lrc20Settings.isEnabled ? 'Enabled' : 'Disabled'
-          } LRC-20`}
-          switchPageName={'lrc20Settings'}
-          showDescription={true}
-          settingDescription={
-            'LRC-20 is Spark’s native token. Enabling LRC-20 lets you send and receive tokens on the Spark network.\n\nBlitz is a Bitcoin focused wallet. We do not promote or endorse the use of tokens. This feature exists because we believe users should have the freedom to use the technology they want to use. If you decide to use tokens, please understand they are speculative and have no real value.'
-          }
-          handleSubmit={() =>
-            toggleMasterInfoObject({
-              lrc20Settings: {
-                ...lrc20Settings,
-                isEnabled: !lrc20Settings.isEnabled,
-              },
-            })
-          }
-          toggleSwitchStateValue={lrc20Settings.isEnabled}
-        />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <SettingsItemWithSlider
+            settingsTitle={`${
+              lrc20Settings.isEnabled ? 'Enabled' : 'Disabled'
+            } LRC-20`}
+            switchPageName={'lrc20Settings'}
+            showDescription={true}
+            settingDescription={`LRC-20 is Spark’s native token. Enabling LRC-20 allows you send and receive tokens on the Spark network.\n\nBlitz is a Bitcoin focused wallet. We do not promote or endorse the use of tokens. This feature exists because we believe users should have the freedom to use the technology they want to use.\n\nBlitz applies a ${displayCorrectDenomination(
+              {amount: 5, masterInfoObject, fiatStats},
+            )} fee to all token transactions, with 20% of that fee donated to support open-source Bitcoin development.`}
+            handleSubmit={() =>
+              toggleMasterInfoObject({
+                lrc20Settings: {
+                  ...lrc20Settings,
+                  isEnabled: !lrc20Settings.isEnabled,
+                },
+              })
+            }
+            toggleSwitchStateValue={lrc20Settings.isEnabled}
+          />
+        </View>
+      </ScrollView>
     </GlobalThemeView>
   );
 }
