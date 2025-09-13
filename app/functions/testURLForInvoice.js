417583da9bf1f23b1005cce47c43e726a7054813
Blake Kaufman <blakekaufman9@gmail.com>
2025-09-13 11:35:46 -0400

    see if merchange qr is in url. If it is return it otherwise its false

--- app/functions/testURLForInvoice.js
+++ app/functions/testURLForInvoice.js
@@ -1,3 +1,4 @@
+import {convertMerchantQRToLightningAddress} from './sendBitcoin/getMerchantAddress';
 export default function testURLForInvoice(data) {
   try {
     const websiteURL = new URL(data);
@@ -7,9 +8,19 @@ export default function testURLForInvoice(data) {
     if (match) {
       console.log('LNURL invoice:', match[0]);
       return match[0];
-    } else {
-      return false;
     }
+
+    //test for moneybadger invoice in web url
+    const moneyBadgerMatch = convertMerchantQRToLightningAddress({
+      qrContent: data,
+      network: process.env.BOLTZ_ENVIRONEMNT,
+    });
+
+    if (moneyBadgerMatch) {
+      return moneyBadgerMatch;
+    }
+
+    return false;
   } catch (err) {
     console.log('test url for invoice error', err);
     return false;
