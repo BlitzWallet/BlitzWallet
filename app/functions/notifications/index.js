2db2d3a7e27f80e883fb221cffb4449258217355
Blake Kaufman <blakekaufman9@gmail.com>
2025-09-27 11:04:38 -0400

    making sure all notifications show in list even if app is open

--- app/functions/notifications/index.js
+++ app/functions/notifications/index.js
@@ -7,6 +7,7 @@ import {
 setNotificationHandler({
   handleNotification: async () => ({
     shouldShowBanner: true,
+    shouldShowList: true,
     shouldPlaySound: true,
     shouldSetBadge: false,
   }),
