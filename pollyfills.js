3fce41e82b34c6c1be5772ab1200a2e34593e3c4
Blake Kaufman <blakekaufman9@gmail.com>
2025-09-29 21:47:49 -0400

    fixing base64 error

--- pollyfills.js
+++ pollyfills.js
@@ -3,7 +3,8 @@ import 'react-native-get-random-values';
 import 'react-native-gesture-handler';
 
 import '@azure/core-asynciterator-polyfill';
-
+//neeed for encription + spark
+import 'react-native-quick-base64';
 import {Buffer} from '@craftzdog/react-native-buffer';
 global.Buffer = Buffer;
 
