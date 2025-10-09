84dd3c20ab040f350addb5f59a8310682a237e5d
Blake Kaufman <blakekaufman9@gmail.com>
2025-10-09 09:47:42 -0400

    remove uneeded flatlist

--- app/components/admin/homeComponents/homeLightning/cusomFlatlist/CustomFlatList.js
+++ app/components/admin/homeComponents/homeLightning/cusomFlatlist/CustomFlatList.js
@@ -1,114 +1,114 @@
-import React, {useCallback, useRef, useState} from 'react';
-import {Platform, RefreshControl, View} from 'react-native';
-import Animated from 'react-native-reanimated';
-import {useCustomFlatListHook} from './useCustomFlatListHooks';
-import {COLORS} from '../../../../../constants';
-import {useGlobalThemeContext} from '../../../../../../context-store/theme';
-import {
-  crashlyticsLogReport,
-  crashlyticsRecordErrorReport,
-} from '../../../../../functions/crashlyticsLogs';
-import {useFocusEffect} from '@react-navigation/native';
-import {useLiquidEvent} from '../../../../../../context-store/liquidEventContext';
-import {useRootstockProvider} from '../../../../../../context-store/rootstockSwapContext';
-import {
-  SPARK_TX_UPDATE_ENVENT_NAME,
-  sparkTransactionsEventEmitter,
-} from '../../../../../functions/spark/transactions';
+// import React, {useCallback, useRef, useState} from 'react';
+// import {Platform, RefreshControl, View} from 'react-native';
+// import Animated from 'react-native-reanimated';
+// import {useCustomFlatListHook} from './useCustomFlatListHooks';
+// import {COLORS} from '../../../../../constants';
+// import {useGlobalThemeContext} from '../../../../../../context-store/theme';
+// import {
+//   crashlyticsLogReport,
+//   crashlyticsRecordErrorReport,
+// } from '../../../../../functions/crashlyticsLogs';
+// import {useFocusEffect} from '@react-navigation/native';
+// import {useLiquidEvent} from '../../../../../../context-store/liquidEventContext';
+// import {useRootstockProvider} from '../../../../../../context-store/rootstockSwapContext';
+// import {
+//   SPARK_TX_UPDATE_ENVENT_NAME,
+//   sparkTransactionsEventEmitter,
+// } from '../../../../../functions/spark/transactions';
 
-function CustomFlatList({style, ...props}) {
-  const {theme, darkModeType} = useGlobalThemeContext();
-  const {startLiquidEventListener} = useLiquidEvent();
-  const {startRootstockEventListener} = useRootstockProvider();
-  const [refreshing, setRefreshing] = useState(false);
-  const flatListRef = useRef(null);
-  const [
-    scrollHandler,
-    styles,
-    stickyElementStyle,
-    topElementStyle,
-    onLayoutHeaderElement,
-    onLayoutTopListElement,
-    onLayoutStickyElement,
-  ] = useCustomFlatListHook();
+// function CustomFlatList({style, ...props}) {
+//   const {theme, darkModeType} = useGlobalThemeContext();
+//   const {startLiquidEventListener} = useLiquidEvent();
+//   const {startRootstockEventListener} = useRootstockProvider();
+//   const [refreshing, setRefreshing] = useState(false);
+//   const flatListRef = useRef(null);
+//   const [
+//     scrollHandler,
+//     styles,
+//     stickyElementStyle,
+//     topElementStyle,
+//     onLayoutHeaderElement,
+//     onLayoutTopListElement,
+//     onLayoutStickyElement,
+//   ] = useCustomFlatListHook();
 
-  const handleRefresh = useCallback(async () => {
-    crashlyticsLogReport(`Running in handle refresh function on homepage`);
-    try {
-      startLiquidEventListener(2);
-      startRootstockEventListener({intervalMs: 30000});
-      sparkTransactionsEventEmitter.emit(SPARK_TX_UPDATE_ENVENT_NAME, {});
-    } catch (err) {
-      console.log('error refreshing on homepage', err);
-      crashlyticsRecordErrorReport(err.message);
-    } finally {
-      setRefreshing(false);
-    }
-  }, []);
+//   const handleRefresh = useCallback(async () => {
+//     crashlyticsLogReport(`Running in handle refresh function on homepage`);
+//     try {
+//       startLiquidEventListener(2);
+//       startRootstockEventListener({intervalMs: 30000});
+//       sparkTransactionsEventEmitter.emit(SPARK_TX_UPDATE_ENVENT_NAME, {});
+//     } catch (err) {
+//       console.log('error refreshing on homepage', err);
+//       crashlyticsRecordErrorReport(err.message);
+//     } finally {
+//       setRefreshing(false);
+//     }
+//   }, []);
 
-  useFocusEffect(
-    useCallback(() => {
-      if (Platform.OS === 'ios')
-        flatListRef.current?.scrollToOffset({offset: 0});
-      return () => {
-        if (Platform.OS == 'android')
-          flatListRef.current?.scrollToOffset({offset: 0});
-      };
-    }, []),
-  );
+//   useFocusEffect(
+//     useCallback(() => {
+//       if (Platform.OS === 'ios')
+//         flatListRef.current?.scrollToOffset({offset: 0});
+//       return () => {
+//         if (Platform.OS == 'android')
+//           flatListRef.current?.scrollToOffset({offset: 0});
+//       };
+//     }, []),
+//   );
 
-  const colors = Platform.select({
-    ios: darkModeType && theme ? COLORS.darkModeText : COLORS.primary,
-    android: darkModeType && theme ? COLORS.lightModeText : COLORS.primary,
-  });
+//   const colors = Platform.select({
+//     ios: darkModeType && theme ? COLORS.darkModeText : COLORS.primary,
+//     android: darkModeType && theme ? COLORS.lightModeText : COLORS.primary,
+//   });
 
-  return (
-    <View style={style}>
-      <Animated.View
-        style={[styles.stickyElement, stickyElementStyle]}
-        onLayout={onLayoutStickyElement}>
-        {props.StickyElementComponent}
-      </Animated.View>
+//   return (
+//     <View style={style}>
+//       <Animated.View
+//         style={[styles.stickyElement, stickyElementStyle]}
+//         onLayout={onLayoutStickyElement}>
+//         {props.StickyElementComponent}
+//       </Animated.View>
 
-      <Animated.View
-        style={[styles.topElement, topElementStyle]}
-        onLayout={onLayoutTopListElement}>
-        {props.TopListElementComponent}
-      </Animated.View>
+//       <Animated.View
+//         style={[styles.topElement, topElementStyle]}
+//         onLayout={onLayoutTopListElement}>
+//         {props.TopListElementComponent}
+//       </Animated.View>
 
-      <Animated.FlatList
-        refreshControl={
-          props.frompage === 'sparkWallet' ? undefined : (
-            <RefreshControl
-              colors={[colors]}
-              tintColor={
-                darkModeType && theme ? COLORS.darkModeText : COLORS.primary
-              }
-              refreshing={refreshing}
-              onRefresh={handleRefresh}
-            />
-          )
-        }
-        ref={flatListRef}
-        initialNumToRender={10}
-        maxToRenderPerBatch={5}
-        updateCellsBatchingPeriod={50}
-        showsVerticalScrollIndicator={false}
-        {...props}
-        ListHeaderComponent={
-          <Animated.View onLayout={onLayoutHeaderElement}>
-            {props.HeaderComponent}
-          </Animated.View>
-        }
-        ListHeaderComponentStyle={[
-          props.ListHeaderComponentStyle,
-          styles.header,
-        ]}
-        onScroll={scrollHandler}
-        scrollEventThrottle={16}
-      />
-    </View>
-  );
-}
+//       <Animated.FlatList
+//         refreshControl={
+//           props.frompage === 'sparkWallet' ? undefined : (
+//             <RefreshControl
+//               colors={[colors]}
+//               tintColor={
+//                 darkModeType && theme ? COLORS.darkModeText : COLORS.primary
+//               }
+//               refreshing={refreshing}
+//               onRefresh={handleRefresh}
+//             />
+//           )
+//         }
+//         ref={flatListRef}
+//         initialNumToRender={10}
+//         maxToRenderPerBatch={5}
+//         updateCellsBatchingPeriod={50}
+//         showsVerticalScrollIndicator={false}
+//         {...props}
+//         ListHeaderComponent={
+//           <Animated.View onLayout={onLayoutHeaderElement}>
+//             {props.HeaderComponent}
+//           </Animated.View>
+//         }
+//         ListHeaderComponentStyle={[
+//           props.ListHeaderComponentStyle,
+//           styles.header,
+//         ]}
+//         onScroll={scrollHandler}
+//         scrollEventThrottle={16}
+//       />
+//     </View>
+//   );
+// }
 
-export default CustomFlatList;
+// export default CustomFlatList;
--- app/components/admin/homeComponents/homeLightning/cusomFlatlist/useCustomFlatListHooks.js
+++ app/components/admin/homeComponents/homeLightning/cusomFlatlist/useCustomFlatListHooks.js
@@ -1,114 +1,114 @@
-import {useEffect, useRef, useState} from 'react';
-import {Dimensions} from 'react-native';
+// import {useEffect, useRef, useState} from 'react';
+// import {Dimensions} from 'react-native';
 
-import Animated, {
-  useAnimatedScrollHandler,
-  useSharedValue,
-  interpolate,
-  Extrapolation,
-  useAnimatedStyle,
-} from 'react-native-reanimated';
+// import Animated, {
+//   useAnimatedScrollHandler,
+//   useSharedValue,
+//   interpolate,
+//   Extrapolation,
+//   useAnimatedStyle,
+// } from 'react-native-reanimated';
 
-export const useCustomFlatListHook = () => {
-  const scrollY = useSharedValue(0);
-  const [heights, setHeights] = useState({
-    header: 0,
-    sticky: 0,
-    topList: 0,
-  });
-  const windowHeight = useRef(Dimensions.get('window').height).current;
-  const didMount = useRef(null);
+// export const useCustomFlatListHook = () => {
+//   const scrollY = useSharedValue(0);
+//   const [heights, setHeights] = useState({
+//     header: 0,
+//     sticky: 0,
+//     topList: 0,
+//   });
+//   const windowHeight = useRef(Dimensions.get('window').height).current;
+//   const didMount = useRef(null);
 
-  useEffect(() => {
-    didMount.current = true;
-    return () => (didMount.current = false);
-  }, []);
+//   useEffect(() => {
+//     didMount.current = true;
+//     return () => (didMount.current = false);
+//   }, []);
 
-  const scrollHandler = useAnimatedScrollHandler({
-    onScroll: event => {
-      scrollY.value = event.contentOffset.y;
-    },
-  });
-  // Create animated styles using useAnimatedStyle
-  const stickyElementStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateY: interpolate(
-            scrollY.value,
-            [-windowHeight, heights.header],
-            [windowHeight, -heights.header],
-            Extrapolation.CLAMP,
-          ),
-        },
-      ],
-    };
-  });
+//   const scrollHandler = useAnimatedScrollHandler({
+//     onScroll: event => {
+//       scrollY.value = event.contentOffset.y;
+//     },
+//   });
+//   // Create animated styles using useAnimatedStyle
+//   const stickyElementStyle = useAnimatedStyle(() => {
+//     return {
+//       transform: [
+//         {
+//           translateY: interpolate(
+//             scrollY.value,
+//             [-windowHeight, heights.header],
+//             [windowHeight, -heights.header],
+//             Extrapolation.CLAMP,
+//           ),
+//         },
+//       ],
+//     };
+//   });
 
-  const topElementStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateY: interpolate(
-            scrollY.value,
-            [-windowHeight, heights.header + heights.sticky + heights.topList],
-            [
-              windowHeight,
-              -(heights.header + heights.sticky + heights.topList),
-            ],
-            Extrapolation.CLAMP,
-          ),
-        },
-      ],
-    };
-  });
+//   const topElementStyle = useAnimatedStyle(() => {
+//     return {
+//       transform: [
+//         {
+//           translateY: interpolate(
+//             scrollY.value,
+//             [-windowHeight, heights.header + heights.sticky + heights.topList],
+//             [
+//               windowHeight,
+//               -(heights.header + heights.sticky + heights.topList),
+//             ],
+//             Extrapolation.CLAMP,
+//           ),
+//         },
+//       ],
+//     };
+//   });
 
-  const styles = {
-    header: {
-      marginBottom: heights.sticky + heights.topList,
-    },
-    stickyElement: {
-      left: 0,
-      marginTop: heights.header,
-      position: 'absolute',
-      right: 0,
+//   const styles = {
+//     header: {
+//       marginBottom: heights.sticky + heights.topList,
+//     },
+//     stickyElement: {
+//       left: 0,
+//       marginTop: heights.header,
+//       position: 'absolute',
+//       right: 0,
 
-      zIndex: 2,
-    },
-    topElement: {
-      left: 0,
-      marginTop: heights.header + heights.sticky,
-      position: 'absolute',
-      right: 0,
-      zIndex: 1,
-    },
-  };
+//       zIndex: 2,
+//     },
+//     topElement: {
+//       left: 0,
+//       marginTop: heights.header + heights.sticky,
+//       position: 'absolute',
+//       right: 0,
+//       zIndex: 1,
+//     },
+//   };
 
-  const onLayoutHeaderElement = event => {
-    event.persist();
-    if (!didMount.current) return;
-    setHeights(prev => ({...prev, header: event.nativeEvent.layout.height}));
-  };
+//   const onLayoutHeaderElement = event => {
+//     event.persist();
+//     if (!didMount.current) return;
+//     setHeights(prev => ({...prev, header: event.nativeEvent.layout.height}));
+//   };
 
-  const onLayoutTopListElement = event => {
-    event.persist();
-    if (!didMount.current) return;
-    setHeights(prev => ({...prev, topList: event.nativeEvent.layout.height}));
-  };
+//   const onLayoutTopListElement = event => {
+//     event.persist();
+//     if (!didMount.current) return;
+//     setHeights(prev => ({...prev, topList: event.nativeEvent.layout.height}));
+//   };
 
-  const onLayoutTopStickyElement = event => {
-    event.persist();
-    if (!didMount.current) return;
-    setHeights(prev => ({...prev, sticky: event.nativeEvent.layout.height}));
-  };
+//   const onLayoutTopStickyElement = event => {
+//     event.persist();
+//     if (!didMount.current) return;
+//     setHeights(prev => ({...prev, sticky: event.nativeEvent.layout.height}));
+//   };
 
-  return [
-    scrollHandler,
-    styles,
-    stickyElementStyle,
-    topElementStyle,
-    onLayoutHeaderElement,
-    onLayoutTopListElement,
-    onLayoutTopStickyElement,
-  ];
-};
+//   return [
+//     scrollHandler,
+//     styles,
+//     stickyElementStyle,
+//     topElementStyle,
+//     onLayoutHeaderElement,
+//     onLayoutTopListElement,
+//     onLayoutTopStickyElement,
+//   ];
+// };
