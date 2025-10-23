160b388bee8fec2cc297d9f4653fca60c570cd59
Blake Kaufman <blakekaufman9@gmail.com>
2025-10-23 20:27:34 +0200

    guarding navigate to prevent crash

--- app/functions/openWebBrowser.js
+++ app/functions/openWebBrowser.js
@@ -1,10 +1,14 @@
 import * as WebBrowser from 'expo-web-browser';
 
-export default async function openWebBrowser({navigate, link}) {
+export default async function openWebBrowser({ navigate, link }) {
   try {
     await WebBrowser.openBrowserAsync(link);
   } catch (err) {
-    navigate.navigate('ErrorScreen', {errorMessage: 'Cannot open web broswer'});
+    if (navigate) {
+      navigate.navigate('ErrorScreen', {
+        errorMessage: 'Cannot open web broswer',
+      });
+    }
     console.log(err, 'OPENING LINK ERROR');
   }
 }
