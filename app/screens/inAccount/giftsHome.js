12814fbffffbae636e0a7dc557906c9399f083c7
Blake Kaufman <blakekaufman9@gmail.com>
2025-11-19 07:06:46 -0500

    gifts index page

---
+++ app/screens/inAccount/giftsHome.js
@@ -0,0 +1,203 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { ThemeText } from '../../functions/CustomElements';
+import { useGlobalInsets } from '../../../context-store/insetsProvider';
+import GetThemeColors from '../../hooks/themeColors';
+import { CENTER, COLORS, ICONS, SIZES } from '../../constants';
+import { useGlobalThemeContext } from '../../../context-store/theme';
+import ProfileImageSettingsNavigator from '../../functions/CustomElements/profileSettingsNavigator';
+import { useTranslation } from 'react-i18next';
+import { WINDOWWIDTH } from '../../constants/theme';
+
+import GiftsOverview from '../../components/admin/homeComponents/gifts/giftsOverview';
+import ClaimGiftHome from '../../components/admin/homeComponents/gifts/claimGiftHome';
+import ReclaimGift from '../../components/admin/homeComponents/gifts/reclaimGift';
+
+export default function GiftsPageHome() {
+  const [currentView, setCurrentView] = useState('overview');
+  const [activeTab, setActiveTab] = useState('my-gifts');
+  const { topPadding } = useGlobalInsets();
+  const { backgroundColor, backgroundOffset, textColor } = GetThemeColors();
+  const { theme, darkModeType } = useGlobalThemeContext();
+  const { t } = useTranslation();
+
+  const handleTabChange = value => {
+    setActiveTab(value);
+    if (value === 'my-gifts') setCurrentView('overview');
+    if (value === 'claim') setCurrentView('claim');
+    if (value === 'reclaim') setCurrentView('reclaim');
+  };
+
+  const headerBackground = theme ? backgroundOffset : COLORS.darkModeText;
+
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      {/* Header */}
+      <View
+        style={[
+          styles.topBarContainer,
+          { paddingTop: topPadding, backgroundColor: headerBackground },
+        ]}
+      >
+        <View style={styles.topBar}>
+          <ThemeText
+            CustomNumberOfLines={1}
+            content={t('screens.inAccount.giftPages.giftsHome.title')}
+            styles={styles.headerText}
+          />
+          <View style={{ marginLeft: 'auto' }}>
+            <ProfileImageSettingsNavigator />
+          </View>
+        </View>
+      </View>
+
+      {/* Tabs */}
+      <View
+        style={[
+          styles.tabContainer,
+          {
+            backgroundColor: headerBackground,
+            borderBottomColor: backgroundOffset,
+          },
+        ]}
+      >
+        <TouchableOpacity
+          style={[
+            styles.tab,
+            activeTab === 'my-gifts' && {
+              ...styles.activeTab,
+              borderBottomColor: textColor,
+            },
+          ]}
+          onPress={() => handleTabChange('my-gifts')}
+        >
+          <ThemeText
+            styles={[
+              styles.tabText,
+              activeTab === 'my-gifts' && styles.activeTabText,
+            ]}
+            content={t('screens.inAccount.giftPages.giftsHome.myGifts')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.tab,
+            activeTab === 'claim' && {
+              ...styles.activeTab,
+              borderBottomColor: textColor,
+            },
+          ]}
+          onPress={() => handleTabChange('claim')}
+        >
+          <ThemeText
+            styles={[
+              styles.tabText,
+              activeTab === 'claim' && styles.activeTabText,
+            ]}
+            content={t('constants.claim')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.tab,
+            activeTab === 'reclaim' && {
+              ...styles.activeTab,
+              borderBottomColor: textColor,
+            },
+          ]}
+          onPress={() => handleTabChange('reclaim')}
+        >
+          <ThemeText
+            styles={[
+              styles.tabText,
+              activeTab === 'reclaim' && styles.activeTabText,
+            ]}
+            content={t('constants.reclaim')}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* Content */}
+      {currentView === 'overview' && (
+        <GiftsOverview
+          headerBackground={headerBackground}
+          theme={theme}
+          darkModeType={darkModeType}
+        />
+      )}
+      {currentView === 'claim' && (
+        <ClaimGiftHome theme={theme} darkModeType={darkModeType} />
+      )}
+      {currentView === 'reclaim' && (
+        <ReclaimGift theme={theme} darkModeType={darkModeType} t />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topBarContainer: {
+    width: '100%',
+  },
+  topBar: {
+    width: WINDOWWIDTH,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: 40,
+    ...CENTER,
+  },
+  headerText: {
+    flexShrink: 1,
+    width: '100%',
+    textAlign: 'center',
+    fontSize: SIZES.large,
+    paddingHorizontal: 45,
+    position: 'absolute',
+  },
+  header: {
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  backButton: {
+    marginRight: 12,
+  },
+  backText: {
+    fontSize: 16,
+    color: '#555',
+  },
+  headerTitle: {
+    width: '100%',
+    flexShrink: 1,
+    fontSize: SIZES.large,
+    textAlign: 'center',
+    backgroundColor: 'red',
+    position: 'absolute',
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+  },
+  tab: {
+    flex: 1,
+    paddingVertical: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  activeTab: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
+  },
+
+  activeTabText: {
+    fontWeight: '500',
+  },
+});
