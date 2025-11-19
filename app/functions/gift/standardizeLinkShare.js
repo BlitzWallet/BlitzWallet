983906641f215240e06386f9f906b26a482b8ec7
Blake Kaufman <blakekaufman9@gmail.com>
2025-11-19 10:41:12 -0500

    fixed translations issue

--- app/functions/gift/standardizeLinkShare.js
+++ app/functions/gift/standardizeLinkShare.js
@@ -3,7 +3,7 @@ import { BITCOIN_SATS_ICON } from '../../constants';
 import i18n from 'i18next';
 
 export async function handleGiftCardShare({ amount, giftLink }) {
-  const message = i18n.t('giftPages.shareMessage', {
+  const message = i18n.t('screens.inAccount.giftPages.shareMessage', {
     icon: BITCOIN_SATS_ICON,
     amount,
     link: giftLink,
