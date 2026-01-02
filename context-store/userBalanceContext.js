f1febaf06ff688948ba94e62fc082bbe4159dac7
Blake Kaufman <blakekaufman9@gmail.com>
2026-01-02 09:10:22 -0500

    creating dollar sat, dollar token values

--- context-store/userBalanceContext.js
+++ context-store/userBalanceContext.js
@@ -2,26 +2,41 @@ import React, { createContext, useMemo } from 'react';
 
 import { useSparkWallet } from './sparkContext';
 import { useFlashnet } from './flashnetContext';
+import { USDB_TOKEN_ID } from '../app/constants';
+import formatTokensNumber from '../app/functions/lrc20/formatTokensBalance';
 
 // Create a context for the WebView ref
 const UserBalanceContext = createContext(null);
 
 export const UserBalanceProvider = ({ children }) => {
-  const { sparkInformation, USD_BALANCE } = useSparkWallet();
+  const { sparkInformation } = useSparkWallet();
   const { flatnet_sats_per_dollar } = useFlashnet();
 
-  const bitcoinBalance = sparkInformation.balance;
-  const dollarBalance = USD_BALANCE * flatnet_sats_per_dollar;
+  const usdbTokenInfo = sparkInformation?.tokens?.[USDB_TOKEN_ID];
 
-  const totalSatValue = bitcoinBalance + dollarBalance;
+  const dollarBalanceToken = useMemo(() => {
+    return usdbTokenInfo
+      ? formatTokensNumber(
+          usdbTokenInfo?.balance,
+          usdbTokenInfo?.tokenMetadata?.decimals,
+        )
+      : 0;
+  }, [usdbTokenInfo]);
+
+  const bitcoinBalance = sparkInformation.balance;
+
+  const dollarBalanceSat = dollarBalanceToken * flatnet_sats_per_dollar;
+
+  const totalSatValue = bitcoinBalance + dollarBalanceSat;
 
   const contextValue = useMemo(() => {
     return {
       bitcoinBalance,
-      dollarBalance,
+      dollarBalanceSat,
       totalSatValue,
+      dollarBalanceToken,
     };
-  }, [bitcoinBalance, dollarBalance, totalSatValue]);
+  }, [bitcoinBalance, dollarBalanceToken, totalSatValue, dollarBalanceSat]);
 
   return (
     <UserBalanceContext.Provider value={contextValue}>
