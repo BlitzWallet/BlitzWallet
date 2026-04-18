16d168850a0a594fd784070fb893681362c27628
Blake Kaufman <blakekaufman9@gmail.com>
2026-04-18 12:24:27 -0400

    use utils package directly

--- app/functions/checkGoogleServices.js
+++ app/functions/checkGoogleServices.js
@@ -1,14 +1,9 @@
-import {getApp} from '@react-native-firebase/app';
+import { utils } from '@react-native-firebase/app';
 
 export function checkGooglePlayServices() {
   try {
-    const app = getApp();
-
-    console.log(app.utils());
-
     const areGoogleServicesEnabled =
-      app.utils().playServicesAvailability.isAvailable;
-
+      utils().playServicesAvailability.isAvailable;
     return areGoogleServicesEnabled;
   } catch (err) {
     console.log('Error getting google services information', err);
