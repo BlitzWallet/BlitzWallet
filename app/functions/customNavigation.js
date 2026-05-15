cb1c0f74588a3516ff53aa5ee97e7dfd28e88286
Blake Kaufman <blakekaufman9@gmail.com>
2026-05-15 15:29:31 -0400

    removing double tap for contacts page

--- app/functions/customNavigation.js
+++ app/functions/customNavigation.js
@@ -1,18 +1,11 @@
-import { KEYBOARDTIMEOUT } from '../constants/styles';
 import { KeyboardController } from 'react-native-keyboard-controller';
 
-export function keyboardGoBack(navigate) {
-  setTimeout(
-    navigate.goBack,
-    KeyboardController.isVisible() ? KEYBOARDTIMEOUT : 0,
-  );
-  KeyboardController.dismiss();
+export async function keyboardGoBack(navigate) {
+  await KeyboardController.dismiss();
+  setTimeout(navigate.goBack, KeyboardController.isVisible() ? 60 : 0);
 }
 
-export function keyboardNavigate(navigatorFunction) {
-  setTimeout(
-    navigatorFunction,
-    KeyboardController.isVisible() ? KEYBOARDTIMEOUT : 0,
-  );
-  KeyboardController.dismiss();
+export async function keyboardNavigate(navigatorFunction) {
+  await KeyboardController.dismiss();
+  setTimeout(navigatorFunction, KeyboardController.isVisible() ? 60 : 0);
 }
